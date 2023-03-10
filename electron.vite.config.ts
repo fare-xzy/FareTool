2a66331e8fe58d66c2619725d1cd3d556ce183f9
xuziyu <1269131995@qq.com>
2023-03-10 23:49:11 +0800

    引入样式以及路由

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -2,6 +2,13 @@ import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import vue from '@vitejs/plugin-vue'
 
+// 自动导入vue中hook reactive ref等
+import AutoImport from "unplugin-auto-import/vite"
+//自动导入ui-组件 比如说ant-design-vue  element-plus等
+import Components from 'unplugin-vue-components/vite'
+//ant-design-vue
+import {AntDesignVueResolver} from "unplugin-vue-components/resolvers"
+
 export default defineConfig({
   main: {
     plugins: [externalizeDepsPlugin()]
@@ -15,6 +22,20 @@ export default defineConfig({
         '@renderer': resolve('src/renderer/src')
       }
     },
-    plugins: [vue()]
+    plugins: [
+      vue(),
+      AutoImport({
+        //安装两行后你会发现在组件中不用再导入ref，reactive等
+        imports: ['vue', 'vue-router'],
+        dts: "src/auto-import.d.ts",
+        //ant-design-vue
+        resolvers: [AntDesignVueResolver()]
+      }),
+      Components({
+        dirs: ['src/components'],
+        //ant-design-vue   importStyle = false 样式就没了
+        resolvers: [AntDesignVueResolver({importStyle: true, resolveIcons: true})],
+      })
+    ]
   }
 })
